e0b5f3ec935d62781992f16d613229530b9ff0a1
StepanGulyaev <stepan.gulyaev.02@gmail.com>
2024-05-24 03:47:14 +0300

    Cli fixed

--- cli/src/utils/call_sql.py
+++ cli/src/utils/call_sql.py
@@ -1,6 +1,7 @@
 import psycopg
 import os
 import math
+import subprocess
 from click import BadParameter
 from prettytable import PrettyTable, from_db_cursor
 from prettytable import NONE
@@ -22,7 +23,6 @@ from src.config.width_settings import(
     message_part
 )
 
-
 def get_filtered_n_logs(host, *args) -> PrettyTable:
     if not host:
         host = DB_HOST_DEFAULT
@@ -39,7 +39,8 @@ def get_filtered_n_logs(host, *args) -> PrettyTable:
             table = from_db_cursor(req)
             table.align = "l"
             table.vrules = NONE
-            screen_width=os.system('tputs col')
+            screen_width=subprocess.run(['tput','cols'],capture_output=True,text=True)
+            screen_width=float(screen_width.stdout)
             table._max_width={"_id" : math.floor(id_part*screen_width),
                                 "_timestamp" : math.floor(timestamp_part*screen_width),
                                 "_level" : math.floor(timestamp_part*screen_width),
@@ -48,3 +49,46 @@ def get_filtered_n_logs(host, *args) -> PrettyTable:
                                 "_pid" : math.floor(pid_part*screen_width),
                                 "_message": math.floor(message_part*screen_width)}
             return table
+
+
+def get_num_of_messages_by_level(host, *args) -> PrettyTable:
+    if not host:
+        host = DB_HOST_DEFAULT
+    if len(args) != 4:
+        raise BadParameter("You must specify all arguments!")
+    with psycopg.connect(
+        f"dbname={DB_NAME_DEFAULT} user={DB_USER_DEFAULT} password={DB_PASSWORD_DEFAULT} host={host}"
+    ) as conn:
+        with conn.cursor() as cur:
+            query = "SELECT * FROM syslog_ng.logs.prc_get_stat_table_of_warnings(%s, %s, %s, %s);"
+            req = cur.execute(
+                query, args
+            )
+            table = from_db_cursor(req)
+            table.align = "l"
+            return table
+
+
+def get_messages_stat(host, *args) -> PrettyTable:
+    if not host:
+        host = DB_HOST_DEFAULT
+    if len(args) != 7:
+        raise BadParameter("You must specify all arguments!")
+    with psycopg.connect(
+        f"dbname={DB_NAME_DEFAULT} user={DB_USER_DEFAULT} password={DB_PASSWORD_DEFAULT} host={host}"
+    ) as conn:
+        with conn.cursor() as cur:
+            query = "SELECT * FROM syslog_ng.logs.prc_get_n_messages(%s ,%s, %s, %s, %s, %s, %s);"
+            req = cur.execute(
+                query, args
+            )
+            table = from_db_cursor(req)
+            table.align = "l"
+            table.vrules = NONE
+            screen_width=subprocess.run(['tput','cols'],capture_output=True,text=True)
+            screen_width=float(screen_width.stdout)
+            table._max_width={"number_of_appear" : math.floor(0.05*screen_width),
+                              "message" : math.floor(0.95*screen_width)}
+            return table
+
+
